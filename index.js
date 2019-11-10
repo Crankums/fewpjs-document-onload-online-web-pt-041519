860187f1ca04bdf543d036cb6d8c72a98db74e94
Craig Donavin <crdonavin@gmail.com>
2019-11-10 02:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function(){
   console.log("This is really cool"
-  return "This is really cool")
+  return "This is really cool"
 })
